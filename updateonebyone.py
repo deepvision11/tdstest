a458814394d799084ca2416d0ba42b96f7ecec88
deepvision11 <143866263+deepvision11@users.noreply.github.com>
2025-04-29 19:26:46 +0530

    update

--- updateonebyone.py
+++ updateonebyone.py
@@ -66,7 +66,7 @@ def get_next_unprocessed_record(conn, table_name):
         cursor.execute(f"""
         SELECT pan, name, total_tds
         FROM `{table_name}`
-        WHERE pan_msg IS NULL
+        WHERE pan_msg IS NULL OR pan_msg =0 
         ORDER BY total_tds DESC
         LIMIT 1 OFFSET 0
         """)
